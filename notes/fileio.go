4558bb914d99e842864aff4b32470198aa6caa2a
Reynold Chery <joshbaptiste@yahoo.com>
2019-07-25 21:57:07 +0000

    Working on editing notes

--- notes/fileio.go
+++ notes/fileio.go
@@ -1,6 +1,19 @@
 package notes
 
-func EditNote(index int16, note string) error {
-	fmt.Println("vim-go")
+import "os"
+import "os/exec"
+import "errors"
+
+func EditNote(index int16, note string, input string) error {
+	editor := os.Getenv("EDITOR")
+	if editor == "" {
+		return errors.New("EDITOR env variable not set")
+	}
+
+	if input == "-" {
+		//read from stdin
+		exec.Command("/bin/sh", "-c ", editor, " - ")
+	}
+
 	return nil
 }
