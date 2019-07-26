50ac5111de755d6ed53232186e9b5b9283ec53ef
Reynold Chery <joshbaptiste@yahoo.com>
2019-07-26 01:29:28 -0400

    Updates to fileio

--- notes/fileio.go
+++ notes/fileio.go
@@ -4,15 +4,20 @@ import "os"
 import "os/exec"
 import "errors"
 
-func EditNote(index int16, note string, input string) error {
-	editor := os.Getenv("EDITOR")
-	if editor == "" {
-		return errors.New("EDITOR env variable not set")
-	}
+func AddNote(index int16, note, input string) error {
 
+	editor := os.Getenv("EDITOR")
 	if input == "-" {
 		//read from stdin
 		exec.Command("/bin/sh", "-c ", editor, " - ")
+	}
+
+	return nil
+}
+func EditNote(index int16, note, input string) error {
+	editor := os.Getenv("EDITOR")
+	if editor == "" {
+		return errors.New("EDITOR env variable not set")
 	}
 
 	return nil
