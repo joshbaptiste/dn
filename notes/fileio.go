cae975ab07318ec9f8239d9a5c2b96fc3e18f50f
Reynold Chery <joshbaptiste@yahoo.com>
2019-07-30 00:05:31 -0400

    updates to loadNote function

--- notes/fileio.go
+++ notes/fileio.go
@@ -3,7 +3,25 @@ package notes
 import "os"
 import "os/exec"
 import "errors"
+import "fmt"
+import "log"
 
+var DEFAULT_DNCONFIG_PATH = ".config/dn.conf"
+
+func loadNote(index int16) (string, error) {
+	dnConfig := os.Getenv("DN_CONFIG")
+	if dnConfig == "" {
+		dnConf := fmt.Sprintf("%s/%s", os.Getenv("HOME"), DEFAULT_DNCONFIG_PATH)
+		log.Printf("DN_CONFIG not set opening at default %s", dnConf)
+		fd, err := os.Open(dnConf)
+		if err != nil {
+			log.Printf("Unable to open file %s", dnConf)
+			os.Exit(1)
+		}
+
+	}
+	return "nil", nil
+}
 func AddNote(index int16, note, input string) error {
 
 	editor := os.Getenv("EDITOR")
@@ -14,6 +32,7 @@ func AddNote(index int16, note, input string) error {
 
 	return nil
 }
+
 func EditNote(index int16, note, input string) error {
 	editor := os.Getenv("EDITOR")
 	if editor == "" {
